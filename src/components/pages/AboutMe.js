a99d66abda8203aa803174ca207e63f71b09b263
Roberto <true4321king@gmail.com>
2021-09-28 11:29:11 -0100

    Coding the About me session

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { FiDownload } from 'react-icons/fi'
 
 import ProfileImage from '../../assets/images/perfil.png'
+import CVFile from '../../assets/files/Curriculum_ Roberto_Medina.pdf'
 
 import './About.css'
 
 const AboutMe = () =>{
+    const statisticInfo = [
+      {
+        years:"02+",
+        content:"Years experience"
+      },
+      {
+        years:"20+",
+        content:"Github projects"
+      },
+      {
+        years:"01+",
+        content:"Companies worked"
+      }
+    ]
     return (
       <div className="AboutMe" id="AboutMe">
         <div className="Title">
@@ -12,8 +28,31 @@ const AboutMe = () =>{
           <h2>Short Description</h2>
         </div>
         <div className="AboutmeConteiner">
-          <div>
-            <p>ko</p>
+          <div className="Conteiner-1">
+            <div className="AboutText">
+              <p>High level of software development,
+                leading throughout its life cycle, 
+                from the development of the back-end to the 
+                front-end. And also experienced in systems 
+                development.
+              </p>
+            </div>
+            <div className="AboutStatistic">
+              <ul>
+                {statisticInfo.map((item, i) =>
+                  <li key={i}>
+                    <h1>{item.years}</h1>
+                    <p>{item.content}</p>
+                  </li>
+                )}
+              </ul>
+            </div>
+            <div className="CVDownloader">
+                <a download="" href={CVFile}>
+                  Download
+                  <FiDownload  className="DownloadIcon"/>
+                </a>
+            </div>
             <div>
               <img src={ProfileImage} 
                    alt="profile" 
